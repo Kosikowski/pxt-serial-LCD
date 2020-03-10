1f817fa84cb1d2e6d1a4145e6488c61fe3e33e0f
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2020-03-10 09:49:37 +0100

    first release

--- README.md
+++ README.md
@@ -17,14 +17,10 @@ Um dieses Repository in MakeCode zu bearbeiten.
 * klicke auf **Importieren** und dann auf **Importiere URL**
 * füge **https://github.com/mkleinsb/pxt-serial-lcd** ein und klicke auf Importieren
 
-## Blockvorschau
-
-Dieses Bild zeigt den Blockcode vom letzten Commit im Master an.
-Die Aktualisierung dieses Bildes kann einige Minuten dauern.
-
-![Eine gerenderte Ansicht der Blöcke](https://github.com/mkleinsb/pxt-serial-lcd/raw/master/.github/makecode/blocks.png)
 
 #### Metadaten (verwendet für Suche, Rendering)
 
 * for PXT/microbit
+* for PXT/calliope
+
 <script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
--- test.ts
+++ test.ts
@@ -1,1 +1,21 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+input.onButtonPressed(Button.B, function () {
+    serialLCD.ClearScreen(serialLCD.LcdColor(LCD_COLOR.White))
+})
+input.onButtonPressed(Button.A, function () {
+    serialLCD.Dis16String(0, 0, serialLCD.LcdColor(LCD_COLOR.Red), serialLCD.LcdColor(LCD_COLOR.White), "Test16")
+    serialLCD.Dis24String(0, 16, serialLCD.LcdColor(LCD_COLOR.Green), serialLCD.LcdColor(LCD_COLOR.White), "Test24")
+    serialLCD.Dis32String(0, 40, serialLCD.LcdColor(LCD_COLOR.Cyan), serialLCD.LcdColor(LCD_COLOR.White), "Test40")
+    serialLCD.Dis64String(0, 72, serialLCD.LcdColor(LCD_COLOR.Blue), serialLCD.LcdColor(LCD_COLOR.White), "Test64")
+    for (let Index = 0; Index <= 100; Index++) {
+        serialLCD.DrawPoint(Index, Index, serialLCD.LcdColor(LCD_COLOR.Magenta))
+    }
+    for (let Index = 0; Index <= 100; Index++) {
+        serialLCD.DrawFill(Index + 100, 100 - Index, 2, 2, serialLCD.LcdColor(LCD_COLOR.Blue))
+    }
+})
+serial.redirect(
+SerialPin.P1,
+SerialPin.P2,
+BaudRate.BaudRate57600
+)
